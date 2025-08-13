1be6b1a7742f8a50610b363d8050296d3dd65494
Andrin-Heinis <andrin.heinis@liip.ch>
2025-08-13 09:59:47 +0200

    Netlify: generate + netlify-static preset

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,4 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  nitro: { preset: 'netlify-static' }
+  // wenn du Docus nutzt: extends: '@nuxt-themes/docus'
 })
